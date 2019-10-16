f47655a9b43678f8e59017d97a7500aff6ecef35
reggierabit188 <reggierabit@yandex.ru>
2019-10-16 12:05:55 +0300

    Task31 переделать

--- task1.py
+++ task1.py
@@ -1,3 +1,12 @@
-def a, b ("a", "b")
-    return a//b
-    if
+def func(y, z):
+    return y // z
+
+a = int(input("Enter first values - "))
+if y <= 0:
+    y = int(print("Делить на нуль нельзя "))
+else:
+    z = int(input("Enter second values - "))
+if z <= 0:
+    print("Делить на нуль нельзя ")
+else:
+    print(func(y, z))
